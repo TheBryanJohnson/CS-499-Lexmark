868b3ca0e2d77a888ee8b0ff227a68edd3928ba1
Cody Hacker <codyhacker24@gmail.com>
2018-10-18 13:47:09 -0400

    Added a way to specify output size.

--- prg/parseCLI.py
+++ prg/parseCLI.py
@@ -5,6 +5,9 @@ def parseCLI():
     parser = argparse.ArgumentParser(description="Improve the quality of a compressed image.")
     parser.add_argument("input file path", metavar="Input", type=str, nargs=1, help="An image file that you wish to process.")
     parser.add_argument("output file path", metavar="Output", type=str, nargs=1, help="The location where you would like the output saved")
+    parser.add_argument("-s", "--size", metavar="Size", choices=["orig", "100", "150", "300"], default="300",
+    help="Specifies the size of the output image in dots per inch (dpi)")
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1")
     args = parser.parse_args()
     return args
 
