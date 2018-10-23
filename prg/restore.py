da04a3b05cc85bf78906a5c2c49318a0a8b35fb7
Cody Hacker <codyhacker24@gmail.com>
2018-10-23 16:11:59 -0400

    Added call to ImageRestore class.

--- prg/restore.py
+++ prg/restore.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import os
 from parseCLI import parseCLI
+from ImageRestore import ImageRestore
 
 def main():
     args = parseCLI()
     print(args)
+    ir = ImageRestore()
+    ir.prepare("test.png")
     print("Hello, World!")
 
 if __name__ == "__main__":
