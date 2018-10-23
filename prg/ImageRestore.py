ed1f24581fe28d8840304be942ec2d6127a71e46
Cody Hacker <codyhacker24@gmail.com>
2018-10-23 16:13:42 -0400

    Added init, prepare, and createPixelArray.  Class now runs without errors.

--- prg/ImageRestore.py
+++ prg/ImageRestore.py
@@ -1,19 +1,31 @@
 #!/bin/python3
 
+import numpy as np
 from PIL import Image
 
 class ImageRestore:
-    inImage
-    outImage
+    def __init__(self):
+        self.inImage = Image.new('RGBA', [0, 0])
+        self.outImage = Image.new('RGBA', [0, 0])
+        self.pixelArray = np.empty((0,0), np.int8)
 
-    def openImage(filename):
+    def prepare(self, filename):
+        self.openImage(filename)
+        self.convertToGreyscale()
+        self.createPixelArray()
+
+    def openImage(self, filename):
         try:
-            inImage = Image.open(filename)
+            self.inImage = Image.open(filename)
         except IOError:
             print("Error:  Input file path not valid")
             return -1
     
-    def convertToGreyscale():
-        inImage = inImage.convert('L')
+    def convertToGreyscale(self):
+        self.inImage = self.inImage.convert('L')
 
-    
+    def createPixelArray(self):
+        self.pixelArray = np.fromstring(self.inImage.tobytes(), dtype=np.uint8)
+        self.pixelArray = self.pixelArray.reshape((self.inImage.size[0], self.inImage.size[1]))
+        for i in self.pixelArray:
+            print(i)
