e182b2a437d36b0164712e657d443172c4fe56dd
Cody Hacker <codyhacker24@gmail.com>
2018-10-23 13:59:24 -0400

    Added openImage method

--- prg/ImageRestore.py
+++ prg/ImageRestore.py
@@ -1,9 +1,15 @@
 #!/bin/python3
 
-#from PIL import Image
+from PIL import Image
 
 class ImageRestore:
     inImage
     outImage
 
-    
+    def openImage(filename):
+        try:
+            inImage = Image.open(filename)
+        except IOError:
+            print("Error:  Input file path not valid")
+            return -1
+        
