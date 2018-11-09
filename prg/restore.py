0200ec49980fe1d32d9db6bb91f009fd6e611c46
Cody Hacker <codyhacker24@gmail.com>
2018-11-09 18:22:58 -0500

    added comments and removed some statements used for debugging.

--- prg/restore.py
+++ prg/restore.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-import os
 from parseCLI import parseCLI
 from ImageRestore import ImageRestore
 
 def main():
+    #get arguments from CLI
     args = parseCLI()
+
+    #store arguments into variables for future usage
     inputFilePath = getattr(args, 'input file path')[0]
     outputFilePath = getattr(args, 'output file path')[0]
     outputSize = getattr(args, 'size')
     resample = getattr(args, 'resample')
     filterName = getattr(args, "filter")
+
+    #instantiate image restore object
     ir = ImageRestore()
+
+    #image preprocessing, exits program if image fails to load properly
     if (ir.prepare(inputFilePath, outputSize, resample, filterName) < 0):
         return -1
+
     #call machine learning method
     ir.outputImage(outputFilePath)
-    print(args)
-    print("End of main")        #debug
+
+    #print(args)                 #debug
+    #print("End of main")        #debug
 
 if __name__ == "__main__":
     main()
